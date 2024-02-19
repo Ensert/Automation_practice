25a132c320e558dfccb2a55ed7ca2da1a02bff5f
Ensert <makcmaksymiv@gmail.com>
2024-02-19 13:03:22 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- upl_cloudflare_bypass.py
+++ upl_cloudflare_bypass.py
@@ -6,14 +6,14 @@ from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome()
-driver.get('https://play.upland.me/login')
+driver.get('*******')
 time.sleep(4)
 #go_to_log = driver.find_element(By.XPATH, '/html/body/div[1]/div[4]/div/div/div/div[2]/div[1]/div/div[3]/button').click()
 log = driver.find_element(By.XPATH, '//*[@id="email-field"]')
 time.sleep(1)
-log.send_keys('makcmaksymiv@gmail.com')
+log.send_keys('*******')
 password = driver.find_element(By.XPATH, '//*[@id="root"]/div[9]/div/div/div[2]/div[1]/div/div/div/div/form/div[2]/input')
-password.send_keys('Aemua11111', Keys.ENTER)
+password.send_keys('*****', Keys.ENTER)
 time.sleep(10)
 
 # no resolved yetr
