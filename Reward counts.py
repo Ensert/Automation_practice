1687b22e39a653368341fb407dfdd227cd62b3ee
Ensert <makcmaksymiv@gmail.com>
2025-03-16 17:39:37 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- Reward counts.py
+++ Reward counts.py
@@ -5,7 +5,7 @@ print(min_reward)
 Epoch_reward = Decimal(min_reward  * 60 * 24)
 print(Epoch_reward)
 Year_reward = Decimal(Epoch_reward * 52)
-print(Year_reward)
+print(Year_reward)s
 
 
 # 1340,398449697542664582
